30ae1e6e5e30dc11554092cbdfe97b9d8f0a9154
Rahul Garg <rg@vihu.dev>
2020-06-14 13:27:14 -0700

    Fix dialyzer

--- src/state_channel/v1/blockchain_state_channel_offer_v1.erl
+++ src/state_channel/v1/blockchain_state_channel_offer_v1.erl
@@ -49,7 +49,7 @@ from_packet(Packet, Hotspot, Region) ->
 hotspot(#blockchain_state_channel_offer_v1_pb{hotspot=Hotspot}) ->
     Hotspot.
 
--spec devaddr(offer()) -> binary().
+-spec devaddr(offer()) -> non_neg_integer().
 devaddr(#blockchain_state_channel_offer_v1_pb{devaddr=DevAddr}) ->
     DevAddr.
 
@@ -57,7 +57,7 @@ devaddr(#blockchain_state_channel_offer_v1_pb{devaddr=DevAddr}) ->
 region(#blockchain_state_channel_offer_v1_pb{region=Region}) ->
     Region.
 
--spec packet_hash(offer()) -> atom().
+-spec packet_hash(offer()) -> binary().
 packet_hash(#blockchain_state_channel_offer_v1_pb{packet_hash=PacketHash}) ->
     PacketHash.
 
